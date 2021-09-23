ab6d52e326fe54848b4c42c3c3d0d5b10e94984b
patrik-upspot <patrik@upspot.de>
2021-09-23 19:57:31 +0200

    add Namespace, Version switch
    # Add Namespace variable
    # Add support to switch between version and latest

--- README.md
+++ README.md
@@ -18,24 +18,26 @@
 
 **Required** NAME OF YOUR SERVICE ON RANCHER CLUSTER WHAT YOU WANT DEPLOY.
 
+### `NAMESPACE`
+
+**Required** NAME OF YOUR NAMESPACE ON RANCHER CLUSTER WHERE YOU WANT DEPLOY.
+
 ### `DOCKER_IMAGE`
 
 **Required** URL TO YOUR DOCKER IMAGE (Ex: AWS or DOCKER REGISTRY).
 
-### `DOCKER_IMAGE_LATEST`
-
-**Optional** URL TO YOUR DOCKER IMAGE WITH LATEST TAG.
-
-
+##INFO:
+If you deploy a image with "ghcr.io/organization/example/imageName:**latest**" only active deployments with latest will be updatet. 
+If you deploy a image with "ghcr.io/organization/example/imageName:**x.x.x**" only active deployment with version number will be updated
 ## Example usage
 `````
   
 - name: Rancher Deploy
-  uses: yantadeu/rancher-deploy-action@v0.0.2
+  uses: upspot/rancher-deploy-action@v0.0.4
   env:
-    RANCHER_ACCESS_KEY: 'XXXXXXX'
-    RANCHER_SECRET_KEY: 'XXXXXXX'
+    RANCHER_ACCESS_KEY: ${{ secrets.RANCHER_ACCESS_KEY }}
+    RANCHER_SECRET_KEY: ${{ secrets.RANCHER_SECRET_KEY }}
     RANCHER_URL_API: 'https://rancher.YOUR-DOMAIN.COM/v3'
     SERVICE_NAME: 'myProject'
-    DOCKER_IMAGE: 'xxxxxxx:yyyyyyyy'
-    DOCKER_IMAGE_LATEST: 'xxxxxxx:latest'
+    NAMESPACE: 'test'
+    DOCKER_IMAGE: 'ghcr.io/organization/example/imageName:latest'
--- deploy_to_rancher.py
+++ deploy_to_rancher.py
@@ -1,15 +1,17 @@
 import os
 import sys
 import requests
+import re
 
 
 class DeployRancher:
     def __init__(self, rancher_access_key, rancher_secret_key, rancher_url_api,
-                 rancher_service_name, rancher_docker_image):
+                 rancher_service_name, rancher_namespace, rancher_docker_image):
         self.access_key = rancher_access_key
         self.secret_key = rancher_secret_key
         self.rancher_url_api = rancher_url_api
         self.service_name = rancher_service_name
+        self.namespace = rancher_namespace
         self.docker_image = rancher_docker_image
         self.rancher_deployment_path = ''
         self.rancher_namespace = ''
@@ -18,16 +20,21 @@ class DeployRancher:
     def deploy(self):
         rp = requests.get('{}/projects'.format(self.rancher_url_api), auth=(self.access_key, self.secret_key))
         projects = rp.json()
+
+        isVersion = re.search("[:][0-9].[0-9].[0-9]", self.docker_image)
+        isLatest = re.search("latest", self.docker_image)
+
         for p in projects['data']:
             w_url = '{}/projects/{}/workloads'.format(self.rancher_url_api, p['id'])
             rw = requests.get(w_url, auth=(self.access_key, self.secret_key))
             workload = rw.json()
             for w in workload['data']:
-                if w['name'] == self.service_name:
+                if w['name'] == self.service_name and w['namespaceId'] == self.namespace:
                     self.rancher_workload_url_api = w_url
                     self.rancher_deployment_path = w['links']['self']
                     self.rancher_namespace = w['namespaceId']
                     break
+
             if self.rancher_deployment_path != '':
                 break
 
@@ -48,17 +55,22 @@ class DeployRancher:
             requests.post(self.rancher_workload_url_api,
                           json=config, auth=(self.access_key, self.secret_key))
         else:
-            response['containers'][0]['image'] = self.docker_image
+            actualImage = response['containers'][0]['image']
+            isActualVersion = re.search("[:][0-9].[0-9].[0-9]", actualImage)
+            isActualLatest = re.search("latest", actualImage)
 
-            requests.put(self.rancher_deployment_path + '?action=redeploy',
-                         json=response, auth=(self.access_key, self.secret_key))
-        sys.exit(0)
+            if (isActualVersion and isVersion ) or (isLatest and isActualLatest):
+                response['containers'][0]['image'] = self.docker_image
+
+                requests.put(self.rancher_deployment_path + '?action=redeploy',
+                             json=response, auth=(self.access_key, self.secret_key))
+            sys.exit(0)
 
 
 def deploy_in_rancher(rancher_access_key, rancher_secret_key, rancher_url_api,
-                      rancher_service_name, rancher_docker_image):
+                      rancher_service_name, rancher_namespace, rancher_docker_image):
     deployment = DeployRancher(rancher_access_key, rancher_secret_key, rancher_url_api,
-                               rancher_service_name, rancher_docker_image)
+                               rancher_service_name, rancher_namespace, rancher_docker_image)
     deployment.deploy()
 
 
@@ -68,14 +80,11 @@ if __name__ == '__main__':
     rancher_url_api = os.environ['RANCHER_URL_API']
     rancher_service_name = os.environ['SERVICE_NAME']
     rancher_docker_image = os.environ['DOCKER_IMAGE']
-    rancher_docker_image_latest = os.environ['DOCKER_IMAGE_LATEST']
+    rancher_namespace = os.environ['NAMESPACE']
+
     try:
         deploy_in_rancher(rancher_access_key, rancher_secret_key, rancher_url_api,
-                          rancher_service_name, rancher_docker_image)
-        
-        if rancher_docker_image_latest != None and rancher_docker_image_latest != "":
-            deploy_in_rancher(rancher_access_key, rancher_secret_key, rancher_url_api, 
-                              rancher_service_name, rancher_docker_image_latest)
+                          rancher_service_name, rancher_namespace, rancher_docker_image)
 
     except Exception as e:
         print(e)
